31ebdb587e40d789e6e015a981019b6983a8e619
adityashnkr <adityashankar009@gmail.com>
2021-07-17 16:46:41 +0530

    revert

--- safve/src/components/home.jsx
+++ safve/src/components/home.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-// Hussain idhar home page bana thike me sone jara 30 mins me aata hu {13:16}
+import './home.css' ;
 function Home(){
     return(
-        <div>
-            Hello World
+        <div id="homePage-wrapper" >
+            <div id="NavBar">
+                <div id="nav-right">
+                    <img src="assets/pixil-frame-0.png" id="logo" alt="logo"/>
+                </div> 
+                <div id="nav-left">
+                    <a>Discover</a>
+                    <a>Help</a>
+                    <button class='butt'>Sign In</button>
+                </div>
+            </div>
+            
+            <div id='yo'>
+                <img src='assets/rand1.png' id='rand' alt='ranms'/><center><div id="line"><h1>Get Rewarded to SAVE!</h1>
+                        <button class='butt'>Join Us</button>            
+            </div></center>
+                <img src='assets/rand2.png' id='rand' alt='ranms'/>
+            </div>
+            
         </div>
     )
 }
