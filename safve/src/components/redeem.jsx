27680212be86668f2a34311e18bb3786b82a37f6
adityashnkr <adityashankar009@gmail.com>
2021-07-18 20:19:54 +0530

    redeem

--- safve/src/components/redeem.jsx
+++ safve/src/components/redeem.jsx
@@ -6,19 +6,46 @@ function Redeem(){
     return(
         <React.Fragment>
             <Header />
-            <h1 className="text-center text-capitalize my-5">Invest with our partner to earn cahback on every transaction 💸</h1>
-            <h2 className="text-center  text-capitalize  my-5 font-weight-normal">Get 0.5 stonkoin for every 10$</h2>
-            <h3  className="text-center text-primary text-capitalize text-muted my-5 font-weight-normal">Apes strong together baking tendies with diamond hands💎🙌.</h3>
-            <h4  className="text-center text-primary text-capitalize text-muted my-5 font-weight-light">0 Commissions and no deposit minimums.
-<br/><br/>Everyone gets smart tools for smart investing.Diversifying your portfolio with a comprehensive suite of investment products including stocks, fractional shares, options, ETFs, and ADRs.
-<br/><br/>Different types of brokerage accounts to satisfy your different investment objectives.<br/><br/>Everyone has access to our advanced and fully customizable desktop platform. You can consolidate your watchlists, analyze charts, place orders, and check your positions across all of platforms
-</h4>
-                <img src="assets\pepi-stojanovski-MJSFNZ8BAXw-unsplash.jpg" alt="invest photo" width="100%" height="700"/>
-                <h1 className="text-center text-capitalize my-5">Your support will help add more money to the liquidity for stonkoin</h1>
-            <h4 className="text-center text-primary text-capitalize text-muted my-5 font-weight-normal">Learn how stonkoin works</h4>
-            <div className="text-center"> 
-            <Button href="/stonks" className="btn btn-success">Learn More</Button>
-            </div>
+            <h1 className="text-center text-primary text-capitalize my-5">Redeem 👛</h1>
+            <h4 className="text-center text-primary text-capitalize text-muted my-2 font-weight-light">You can redeem your Stonkoins but we suggest to HODL</h4>
+            <br />
+            <Container>
+            <h2 className="text-left my-5">Redeem:</h2>
+                <Row>
+                    <Col className='sm'>
+                    <Card style={{width: "25rem;"}}>
+                    <img src="assets\paypal.jpg" class="card-img-top" alt="..." />
+                    <CardBody>
+                        <CardTitle tag = "h5">Paypal gift card</CardTitle>
+                        <p class="card-text">Get paypal gift card.</p>
+                        <a href="/quiz" class="btn btn-primary">Reedeem</a>
+                    </CardBody>
+                    </Card>
+                    </Col>
+                    <Col className='sm'>
+                    <Card style={{width: "25rem;"}}>
+                    <img src="assets\pepi-stojanovski-MJSFNZ8BAXw-unsplash.jpg" class="card-img-top" alt="..." />
+                    <CardBody>
+                        <CardTitle tag = "h5">Bank Deposit</CardTitle>
+                        <p class="card-text">Might take 3-5 businees days.</p>
+                        <a href="#" class="btn btn-primary">Reedeem</a>
+                    </CardBody>
+                    </Card>
+                        </Col>
+                        <Col className='sm'>
+                        <Card style={{width: "25rem;"}}>
+                    <img src="assets\coinbase.jpg" class="card-img-top" alt="..." />
+                    <CardBody>
+                        <CardTitle tag = "h5">Crypto</CardTitle>
+                        <p class="card-text">Get BTC to your Coinbase account.</p>
+                        <a href="#" class="btn btn-primary">Reedeem</a>
+                    </CardBody>
+                    </Card>
+                        </Col>
+                </Row>
+                <br /> 
+
+            </Container>
         </React.Fragment>
             )
 }
